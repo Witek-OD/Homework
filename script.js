ca0dd066980b894daf31ce47281059366d5955ea
Wiktor Stoianov <stojanov87@gmail.com>
2024-05-15 00:01:02 +0300

    '4.4'

--- script.js
+++ script.js
@@ -1,38 +1,20 @@
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
 
+switch (true) {
+    case numOrStr===null:
+        console.log('ви скасували');
+        break;
 
-let arr= (+prompt('Пожалуйста введите трёхзначное число')).toString().split('');
+    case numOrStr==='':
+        console.log('Empty String');
+        break;
 
-if(arr.length!=3)
-{
-    alert(`Необходимо ввести ТРЁХЗНАЧНОЕ число!`);
+    case isNaN(+numOrStr):
+        console.log(' number is Ba_NaN');
+        break;
+
+    default:
+        console.log('OK!');
+        break;
 }
-else
-{
-    let result='';
-
-    if((arr[0]==arr[1])&&(arr[0]==arr[2]))
-    {
-        result=`все цифры одинаковы`;
-    }
-    else
-    {
-
-        for (let i =0; i< arr.length;i++)
-        {
-            for (let j=0;j< arr.length;j++)
-            {
-                if((arr[i]==arr[j])&&(i!=j))
-                {
-                    result+=`Цифра ${i+1} совпадает с цифрой ${j+1} \n`;
-                }
-            }
-        }
-        if (result=='')
-        {
-            result='Нет одинаковых цифр';
-        }
-
-    }
-    alert(result);
-}
-
