79eddb0278f22b00c908ff9bc6ac2442ae2c5664
Wiktor Stoianov <stojanov87@gmail.com>
2024-05-17 11:09:35 +0300

    '5.1'

--- script.js
+++ script.js
@@ -1,38 +1,8 @@
+let resultString='';
 
-
-let arr= (+prompt('Пожалуйста введите трёхзначное число')).toString().split('');
-
-if(arr.length!=3)
-{
-    alert(`Необходимо ввести ТРЁХЗНАЧНОЕ число!`);
+for (let i = 20; i <= 30; i += 0.5) {
+    resultString += `${i} `;
 }
-else
-{
-    let result='';
 
-    if((arr[0]==arr[1])&&(arr[0]==arr[2]))
-    {
-        result=`все цифры одинаковы`;
-    }
-    else
-    {
-
-        for (let i =0; i< arr.length;i++)
-        {
-            for (let j=0;j< arr.length;j++)
-            {
-                if((arr[i]==arr[j])&&(i!=j))
-                {
-                    result+=`Цифра ${i+1} совпадает с цифрой ${j+1} \n`;
-                }
-            }
-        }
-        if (result=='')
-        {
-            result='Нет одинаковых цифр';
-        }
-
-    }
-    alert(result);
-}
+console.log(resultString);
 
