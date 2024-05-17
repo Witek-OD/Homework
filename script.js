96b1030c95d1373ce1cf9ab61365adc3446f46a9
Wiktor Stoianov <stojanov87@gmail.com>
2024-05-17 18:50:14 +0300

    '5.4'

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let userNumber=prompt('Пожалуйста введите число');
 var regExp = /^[0-9]+$/g;
 
 if (regExp.test(userNumber)){
+    if ((userNumber===0) || (userNumber===1)) {
+        alert("Числа 0 и 1 не являются ни простыми, ни составными.");
+
+    } else {
     let isSimple=true;
 
     for (let i = 2; i < userNumber; i++) {
@@ -15,7 +19,8 @@ if (regExp.test(userNumber)){
         alert(`Число ${userNumber} является простым`);
 
     } else {
-        alert(`Число ${userNumber} не является простым`);
+        alert(`Число ${userNumber} является составным`);
+    }
     }
 
 } else {
