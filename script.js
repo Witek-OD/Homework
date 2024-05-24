12cea680b6ab53c8cf26f92d4bc8d5f87e6deb36
Wiktor Stoianov <stojanov87@gmail.com>
2024-05-24 23:01:05 +0300

    '7.3'

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
 //#region Function
-function curry(func) {
-     return function(first) {
-        return function(second) {
-            return func(first, second);
-        };
-    };
+
+function validateString(inputString){
+  let regExp = /^[0-9]+$/g;
+
+  return regExp.test(inputString);
 }
 
-function sum(x, y) {
-    return x + y;
+function checkValue() {
+  let curentValue = 0;
+
+  return function(lastValue) {
+    return curentValue = lastValue;
+  }
+
 }
+
 //#endregion
 
-let name = curry(sum);
+let checkingValue = checkValue();
 
-console.log( name(5)(2) );
+let i=0;
+
+do {
+  let inputValue= +prompt('Пожалуйста введите число больше 100');
+
+  if (! validateString(inputValue) ){
+    alert('Пожалуйста в следующий раз введите цифры!');
+
+  } else if ( (inputValue < 100) && (i != 9)) {
+    checkingValue(inputValue);
+
+    i++;
+  } else {
+    console.log( checkingValue(inputValue) );
+
+    break;
+  }
+} while (i<10);
+
+
+
+
