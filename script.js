1f1371b325f7dbaa773caaeb4c8aa62286091ce2
Wiktor Stoianov <stojanov87@gmail.com>
2024-06-06 16:52:35 +0300

    '9.1'

--- script.js
+++ script.js
@@ -1,38 +1,23 @@
+let company ={
+  sales: [{name:'John',salary:1000},{name:'Alice',salary:600}],
+  development:{
+      web:[{name:'Peter',salary:2000},{name:'Alex',salary:1800}],
+      internals:[{name:'Jack',salary:1300}],
+  },
+};
 
+function totalSalaries(currentCompany) {
+  if (Array.isArray(currentCompany)) {
+    return currentCompany.reduce((calculated, newValue) => calculated + newValue.salary, 0);
+  } else {
+    let departmentSalary = 0;
 
-let arr= (+prompt('Пожалуйста введите трёхзначное число')).toString().split('');
-
-if(arr.length!=3)
-{
-    alert(`Необходимо ввести ТРЁХЗНАЧНОЕ число!`);
-}
-else
-{
-    let result='';
-
-    if((arr[0]==arr[1])&&(arr[0]==arr[2]))
-    {
-        result=`все цифры одинаковы`;
+    for (let department of Object.values( currentCompany )) {
+      departmentSalary += totalSalaries(department);
     }
-    else
-    {
 
-        for (let i =0; i< arr.length;i++)
-        {
-            for (let j=0;j< arr.length;j++)
-            {
-                if((arr[i]==arr[j])&&(i!=j))
-                {
-                    result+=`Цифра ${i+1} совпадает с цифрой ${j+1} \n`;
-                }
-            }
-        }
-        if (result=='')
-        {
-            result='Нет одинаковых цифр';
-        }
-
-    }
-    alert(result);
+    return departmentSalary;
+  }
 }
 
+console.log(totalSalaries(company));
